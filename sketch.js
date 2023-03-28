22a2ade7a79646b2ef8b654535f7fa75baa457a2
LyanKaleu <126838026+LyanKaleu@users.noreply.github.com>
2023-03-28 12:59:20 -0300

    Primeiro dia do challenge! #7DaysOfCode
    Assuntos abordados no primeiro dia: tipos de variáveis, operação de igualdade, strings.

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let n1 = 1
+let string1 = '1'
+let n30 = 30
+let string30 = '30'
+let n10 = 10
+let string10 = '10'
+
+if (n1 == string1) {
+  console.log('As variáveis n1 e string1 têm o mesmo valor, porém tipos diferentes')
+}
+else {
+  console.log('As variáveis n1 e string1 não têm o mesmo valor')
+}
+
+if (n30 === string30) {
+  console.log('As variáveis n30 e string30 têm o mesmo valor e mesmo tipo')
+}
+else {
+  console.log('As variáveis n30 e string30 não têm o mesmo tipo')
+}
+
+if (n10 == string10) {
+  console.log('As variáveis n10 e string10 têm o mesmo valor, porém tipos diferentes')
+}
+else {
+  console.log('As variáveis n10 e string10 não têm o mesmo valor')
+}
