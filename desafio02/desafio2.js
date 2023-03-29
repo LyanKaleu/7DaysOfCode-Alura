5fc6616bcba4ff40f2bbc954fe0da5984d478e30
LyanKaleu <126838026+LyanKaleu@users.noreply.github.com>
2023-03-29 11:32:46 -0300

    corrigindo o console log do exercicio opcional

--- desafio02/desafio2.js
+++ desafio02/desafio2.js
@@ -4,7 +4,7 @@ let linguagem = prompt("Qual linguagem de programação você está estudando?")
 
 console.log(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
 
-let gostaDeEstudar = prompt('Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO');
+let gostaDeEstudar = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO`);
 if (gostaDeEstudar == 1) {
     console.log('Muito bom! Continue estudando e você terá muito sucesso.')
 } else if (gostaDeEstudar == 2) {
