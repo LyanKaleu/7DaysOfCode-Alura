bc64e3b2eb7e6892b0d2fa4229c8dfd97a28ef3e
LyanKaleu <lyankaleu12@gmail.com>
2023-03-29 11:14:51 -0300

    exercicio opcional

---
+++ README.md
@@ -0,0 +1,3 @@
+# 7DaysOfCode-Alura
+Desafio de 7 dias da Alura para aprender lógica de programação
+<p>Soluções feitas no site: https://editor.p5js.org</p>
---
+++ desafio02/desafio2.js
@@ -0,0 +1,14 @@
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+console.log(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+let gostaDeEstudar = prompt('Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO');
+if (gostaDeEstudar == 1) {
+    console.log('Muito bom! Continue estudando e você terá muito sucesso.')
+} else if (gostaDeEstudar == 2) {
+    console.log('Ahh que pena... Já tentou aprender outras linguagens?');
+} else {
+    console.log('Resposta inválida. Tente novamente!')
+}
